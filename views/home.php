5c88d2e84c0de392f6577a16f2f032c7de4d1115
lilaugustk <lilaugustk@gmail.com>
2025-07-29 11:12:40 +0700

    code giao diện tĩnh trang chủ

--- views/home.php
+++ views/home.php
@@ -6,27 +6,152 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tiny Room</title>
 </head>
+<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+<link rel="stylesheet" href="styles/style.css">
 
 <body>
-    <table border="1">
-        <tr>
-            <td>ID</td>
-            <td>Tên</td>
-            <td>Giá</td>
-            <td>Ảnh</td>
-            <td>Danh mục</td>
-        </tr>
-
-        <?php foreach ($listProducts as $product) {
-        ?><tr>
-                <td><?= $product["product_id"] ?></td>
-                <td><?= $product["product_name"] ?></td>
-                <td><?= $product["price"] ?></td>
-                <td><?= $product["image"] ?></td>
-                <td><?= $product["category_name"] ?></td>
-            </tr>
-        <?php  } ?>
-    </table>
+    <div class="container">
+        <header>
+            <div class="header-bar">
+                <div class="logo"><img src="img/Logo.png" alt="notfound"></div>
+                <ul>
+                    <li>
+                        <a href="">Trang Chủ</a>
+                    </li>
+                    <li>
+                        <a href="">Sản Phẩm</a>
+                    </li>
+                    <li>
+                        <a href="">Giới Thiệu</a>
+                    </li>
+                    <li>
+                        <a href="">Liên Hệ</a>
+                    </li>
+                </ul>
+                <div class="SCU">
+                    <img src="img/Search.svg" alt="notfound">
+                    <img src="img/Cart.svg" alt="notfound">
+                    <img src="img/User.svg" alt="notfound">
+                </div>
+            </div>
+        </header>
+        <div class="box-banner">
+            <img src="img/Banner.jpg" alt="" class="banner">
+            <div class="head-line">
+                <p class="title-banner">Kiến Tạo Không Gian Sống Của Bạn</p>
+                <p class="slogan">Hãy để chúng tôi biến căn phòng trong mơ của bạn thành hiện thực</p>
+                <div class="button-banner"><a href="about.php">Tìm Hiểu Ngay</a></div>
+            </div>
+        </div>
+        <article>
+            <div class="content-box">
+                <div class="content-title">Sản Phẩm Hot</div>
+                <div class="content-item-box">
+                    <div class="content-item">
+                        <img src="" alt="">
+                        <p class="name"></p>
+                        <span class="price">
+                            <p>Giá:</p>
+                            <p class="price-value">1.000.000đ</p>
+                        </span>
+                    </div>
+                    <div class="content-item">
+                        <img src="" alt="">
+                        <p class="name"></p>
+                        <span class="price">
+                            <p>Giá:</p>
+                            <p class="price-value">1.000.000đ</p>
+                        </span>
+                    </div>
+                    <div class="content-item">
+                        <img src="" alt="">
+                        <p class="name"></p>
+                        <span class="price">
+                            <p>Giá:</p>
+                            <p class="price-value">1.000.000đ</p>
+                        </span>
+                    </div>
+                </div>
+                <div class="button-more-box"> <button class="button-more">Khám Phá</button>
+                </div>
+            </div>
+            <div class="content-box">
+                <div class="content-title">Sản Phẩm Mới</div>
+                <div class="content-item-box">
+                    <div class="content-item">
+                        <img src="" alt="">
+                        <p class="name"></p>
+                        <span class="price">
+                            <p>Giá:</p>
+                            <p class="price-value">1.000.000đ</p>
+                        </span>
+                    </div>
+                    <div class="content-item">
+                        <img src="" alt="">
+                        <p class="name"></p>
+                        <span class="price">
+                            <p>Giá:</p>
+                            <p class="price-value">1.000.000đ</p>
+                        </span>
+                    </div>
+                    <div class="content-item">
+                        <img src="" alt="">
+                        <p class="name"></p>
+                        <span class="price">
+                            <p>Giá:</p>
+                            <p class="price-value">1.000.000đ</p>
+                        </span>
+                    </div>
+                </div>
+                <div class="button-more-box"> <button class="button-more">Khám Phá</button>
+                </div>
+            </div>
+            <div class="content-box">
+                <div class="content-title">Sản Phẩm Xem Nhiều</div>
+                <div class="content-item-box">
+                    <div class="content-item">
+                        <img src="" alt="">
+                        <p class="name"></p>
+                        <span class="price">
+                            <p>Giá:</p>
+                            <p class="price-value">1.000.000đ</p>
+                        </span>
+                    </div>
+                    <div class="content-item">
+                        <img src="" alt="">
+                        <p class="name"></p>
+                        <span class="price">
+                            <p>Giá:</p>
+                            <p class="price-value">1.000.000đ</p>
+                        </span>
+                    </div>
+                    <div class="content-item">
+                        <img src="" alt="">
+                        <p class="name"></p>
+                        <span class="price">
+                            <p>Giá:</p>
+                            <p class="price-value">1.000.000đ</p>
+                        </span>
+                    </div>
+                </div>
+                <div class="button-more-box"> <button class="button-more">Khám Phá</button>
+                </div>
+            </div>
+        </article>
+        <div class="new-letter-box">
+            <img src="" alt="">
+            <div class="new-letter-content-box">
+                <div class="new-letter-content-text">
+                    <p>Tham gia cùng chúng tôi để</p>
+                    <p>Nhận ưu đãi, giảm giá và nhiều ưu đãi độc quyền.</p>
+                </div>
+                <form action="" class="new-letter-email-form">
+                    <input type="email" placeholder="Vui lòng nhập email" class="new-letter-email-input" required>
+                    <button class="new-letter-email-button">Nhận thông báo</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
